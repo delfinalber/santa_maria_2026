f332960a8e2c42f47137bb20d7087140c0d2cca4
delfinalber <delfin.alber@gmail.com>
2026-05-25 15:57:53 -0500

    login 25-05-2026

--- proyecto_santa_maria/password/conexion.php
+++ proyecto_santa_maria/password/conexion.php
@@ -1,4 +1,3 @@
-
 <?php
 
 //Conexion por procedimientos
--- proyecto_santa_maria/password/login.php
+++ proyecto_santa_maria/password/login.php
@@ -2,44 +2,45 @@
 
 include('conexion.php');
 
-$usuario      = isset($_POST['usuario']) ? $_POST['usuario'] : '';
-$contrasena   = isset($_POST['contrasena']) ? $_POST['contrasena'] : '';
-//$sesion_login = true;
-
-ini_set('display_errors', 'off');
-ini_set('display_startup_errors', 'off');
-error_reporting(0);
-
-$url= "https://www.lanacion.com.co/";
-//$url = "http://127.0.0.1/password/carlos/index.html";
-
-
-//$consulta = mysqli_query ($link, "SELECT * FROM users WHERE ususario = '$usuario' AND contrasena = '$contrasena'");
-
-if(isset($link) && $link) {
-	$query = "SELECT * FROM users WHERE usuario_users ='$usuario' AND contrasena_users = '$contrasena'";
-	$q = mysqli_query($link, $query);
-	try{
-		if($q && mysqli_num_rows($q) > 0)
-		{
-			$result = mysqli_fetch_assoc($q);
-			header("Location: https://www.lanacion.com.co/", true, 301);
-			exit();
-			
-			//echo '<script type="text/javascript">
-          //window.location = "http://www.google.com/"
-      //</scrit>';
-
-			//echo "<script>location.href= '$url'</script>";
-
-			//echo "<script> window.location.href = $url; </script>";
-			  //echo "Usuario Valido Correctamente";
-		}else
-			echo "Usuario o Contraseña Erroneos";
-	}catch(Exception $error) {}
-
-	mysqli_close($link);
-} else {
-	echo "Error de conexión a la base de datos";
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+	header('Location: index.html');
+	exit();
 }
+
+$usuario = isset($_POST['usuario']) ? trim($_POST['usuario']) : '';
+$contrasena = isset($_POST['contrasena']) ? trim($_POST['contrasena']) : '';
+
+if ($usuario === '' || $contrasena === '') {
+	echo 'Usuario y contraseña son obligatorios';
+	exit();
+}
+
+if (isset($link) && $link) {
+	$query = 'SELECT id_users FROM users WHERE usuario_users = ? AND contrasena_users = ? LIMIT 1';
+	$stmt = mysqli_prepare($link, $query);
+
+	if (!$stmt) {
+		echo 'Error al preparar la consulta';
+		mysqli_close($link);
+		exit();
+	}
+
+	mysqli_stmt_bind_param($stmt, 'ss', $usuario, $contrasena);
+	mysqli_stmt_execute($stmt);
+	mysqli_stmt_store_result($stmt);
+
+	if (mysqli_stmt_num_rows($stmt) > 0) {
+		header('Location: netflix.html');
+		mysqli_stmt_close($stmt);
+		mysqli_close($link);
+		exit();
+	}
+
+	mysqli_stmt_close($stmt);
+	mysqli_close($link);
+	echo 'Usuario o contraseña incorrectos';
+	exit();
+}
+
+echo 'Error de conexión a la base de datos';
 ?>
